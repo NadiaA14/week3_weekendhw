e6550815a38b1ca311c68846cb8192e9f0fdbbf8
Nadia Al-Murshedy <nadia@Nadias-MacBook-Air.local>
2022-05-22 22:46:51 +0100

    first commit

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,4 +1,16 @@
 from app import app
 from flask import render_template, request
-from models.game import Game, play_game
+from models.game import *
 
+@app.route("/")
+def index():
+    return render_template("index.html", title="Rock, Paper, Scissors")
+
+@app.route("/twoplayers", method=['POST'])
+def play_game():
+    choice_a = request.form["choice_a"]
+    choice_b = request.form["choice_b"]
+    player_1 = Game("Player 1", choice_a)
+    player_2 = Game("Player 2", choice_b)
+
+    return render_template("twoplayers.html", player_1=player_1, player_2=player_2)
